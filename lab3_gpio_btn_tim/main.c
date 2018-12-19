cb0aecb465d53bf11dcd70a1489161272e4755ed
Stepan Chernousov <stigo93@yandex.ru>
2018-12-19 17:30:27 +0300

    #5: lab3 updated (added debounce)

--- lab3_gpio_btn_tim/main.c
+++ lab3_gpio_btn_tim/main.c
@@ -2,6 +2,7 @@
 
 #define NUM_LEDS 3
 
+static uint8_t WAIT_FLAG   = 0;
 static uint8_t WAY_FLAG    = 1; //1 = 1,2,3 ; 0 = 3,2,1
 static uint8_t current_led = 1;
 
@@ -27,6 +28,16 @@ void switch_led (uint8_t way)
   }
 }
 
+void TIM3_IRQHandler (void)
+{
+  if (TIM_GetITStatus (TIM3, TIM_IT_Update) != RESET)
+  {
+    WAIT_FLAG = 0;
+    TIM_ClearITPendingBit (TIM3, TIM_IT_Update);
+    TIM_Cmd (TIM3, DISABLE);
+  }
+}
+
 void TIM2_IRQHandler (void)
 {
   if (TIM_GetITStatus (TIM2, TIM_IT_Update) != RESET)
@@ -41,7 +52,12 @@ void EXTI0_IRQHandler (void)
 {
   if (EXTI_GetITStatus (EXTI_Line0) != RESET)
   {
-    WAY_FLAG = !!(WAY_FLAG != 1);
+	if (WAIT_FLAG == 0)
+	{
+      WAY_FLAG = !!(WAY_FLAG != 1);
+      WAIT_FLAG = 1;
+      TIM_Cmd (TIM3, ENABLE);
+	}
     EXTI_ClearITPendingBit (EXTI_Line0);
   }
 }
@@ -89,15 +105,30 @@ int main(void)
   TIM_InitStructure.TIM_CounterMode = TIM_CounterMode_Up;
   TIM_TimeBaseInit (TIM2, &TIM_InitStructure);
 
+  RCC_APB1PeriphClockCmd (RCC_APB1Periph_TIM3, ENABLE);
+  TIM_InitStructure.TIM_Period = 200 - 1; //100 ms
+  TIM_InitStructure.TIM_Prescaler = 42000 - 1; //500us
+  TIM_InitStructure.TIM_ClockDivision = 0;
+  TIM_InitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+  TIM_TimeBaseInit (TIM3, &TIM_InitStructure);
+
   /* Setup TIM2 interrupt */
-  TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
   NVIC_InitTypeDef NVIC_InitStructure;
+
+  TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
   NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
   NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
   NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
   NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
   NVIC_Init(&NVIC_InitStructure);
  
+  TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
+  NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+
   /* Setup ext interrupts for button */
   RCC_APB2PeriphClockCmd (RCC_APB2Periph_SYSCFG, ENABLE);
   SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE, EXTI_PinSource0);
@@ -120,6 +151,7 @@ int main(void)
   GPIO_SetBits(GPIOA, GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10); //red, green, blue
 
   TIM_Cmd (TIM2, ENABLE);
+  TIM_Cmd (TIM3, ENABLE);
 
   while (1)
   {
